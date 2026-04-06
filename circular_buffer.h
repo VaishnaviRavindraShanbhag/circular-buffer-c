3cf158910155b293ea5cbe1c9b0074c6002abc0c
VaishnaviRavindraShanbhag <vaishnavishanbhag25@gmail.com>
2026-04-06 13:39:08 -0400

    Update circular_buffer.h

--- circular_buffer.h
+++ circular_buffer.h
@@ -1,11 +1,11 @@
-#ifndef CIRCULAR_BUFFER_H
-#define CIRCULAR_BUFFER_H
+#ifndef CIRCULAR_BUFFER_H        // ifndef -> if not defined. This line basically means that is CIRCULAR_BUFFER_H is not defined then go through the code. if it is then skip to endif. This allows for .h files
+#define CIRCULAR_BUFFER_H        // to only be included once. If we skip this then we run the risk of .h files being included more then once which could lead to errors like: redefinition of struct or duplicate function declarations
 
 #include <stdint.h> 
 #include <stdbool.h>
 
 typedef struct {
-    uint8_t * buffer;
+    uint8_t * buffer;        // pointer to an unsigned integer
     int head; 
     int tail; 
     int maxlen;
